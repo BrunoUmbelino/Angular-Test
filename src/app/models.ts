ea52d913b509e57fcf8cfa7b7087b613e16246b7
Bruno <bruno.umbelino0@gmail.com>
2021-09-01 23:37:09 -0300

    improving the interfaces

--- src/app/models.ts
+++ src/app/models.ts
@@ -11,5 +11,39 @@ export interface Game {
   publishers: Array<Publishers>;
   ratings: Array<Rating>;
   screenshots: Array<Screenshots>;
-  trailers: Array<>;
+  trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+  results: Array<T>;
+}
+
+interface Genre {
+  name: string;
+}
+
+interface ParentPlatform {
+  platform: {
+    name: string;
+  };
+}
+
+interface Publishers {
+  name: string;
+}
+
+interface Rating {
+  id: number;
+  count: number;
+  title: string;
+}
+
+interface Screenshots {
+  image: string;
+}
+
+interface Trailer {
+  data: {
+    max: string;
+  };
 }

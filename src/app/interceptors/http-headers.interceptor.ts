71dc7e16e0c848483c3e0da199405628908c3f4f
Bruno Umbelino <bruno.umbelino0@gmai.com>
2021-09-08 14:09:28 -0300

    add more interceptors

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,22 @@
+ 
+var key = 'a9cccbad4ca64aff88b8462cd45b6d49';
+
+
+ @Injectable()
+ export class HttpHeadersInterceptor implements HttpInterceptor {
+     constructor(){
+
+     }
+
+     intercept(
+         req: HttpRequest<any>,
+         next: HttpHandler
+     ): Observable<HttpEvent<any>> {
+         req = req.clone({
+            setHeaders: {
+                'x-rapidapi-key': key,
+                'xrapidapi-host': 'rawg-video-games-database.p.rapidapi.com', 
+            }
+         })
+     }
+ }
